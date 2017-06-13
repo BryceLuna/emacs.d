02ebcf4cf4d496ab57289084e048ca5d1a786c92
bryceluna <bryce.luna@gmail.com>
2017-06-12 20:05:16 -0600

    removed stata, cleaned comment

--- init.el
+++ init.el
@@ -240,12 +240,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
 
- ;;; auctex inverse search https://tex.stackexchange.com/questions/161797/how-to-configure-emacs-and-auctex-to-perform-forward-and-inverse-search
+ ;; auctex inverse search
+ ;; https://tex.stackexchange.com/questions/161797/how-to-configure-emacs-and-auctex-to-perform-forward-and-inverse-search
  '(TeX-source-correlate-method (quote synctex))
  '(TeX-source-correlate-mode t)
  '(TeX-source-correlate-start-server t)
- ;;; Tell Emacs to use stata-se instead of stata
- '(inferior-STA-program-name "stata-se"))
+ ;; Tell Emacs to use stata-se instead of stata
+ ;;'(inferior-STA-program-name "stata-se")
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
